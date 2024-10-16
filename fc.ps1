a0464ee558553c546233561038449428227b7251
jetjo <bingsea@foxmail.com>
2024-10-17 00:45:06 +0800

    chore

--- fc.ps1
+++ fc.ps1
@@ -5,6 +5,8 @@ $RemoteRepo="origin"
 $GitDir=(Get-Item ($LibRelPath | Resolve-Path)).FullName
 $GitDir
 git -C $GitDir switch $Branch
-git -C $GitDir pull $RemoteRepo
+# git -C $GitDir pull $RemoteRepo
+git fetch --all
+git reset --hard $RemoteRepo/$Branch
 npm unlink $LibRelPath
-# npm link $LibRelPath
+npm link $LibRelPath
